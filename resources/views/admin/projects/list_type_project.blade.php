c708c579796a5d26566c80a769948e929648e111
RiccardoLoPresti <riccardolopresti35@gmail.com>
2023-01-24 09:46:50 +0100

    [delete] session msg in list_type view

--- resources/views/admin/projects/list_type_project.blade.php
+++ resources/views/admin/projects/list_type_project.blade.php
@@ -10,12 +10,6 @@
     <div class="row justify-content-center">
         <div class="px-5 py-3">
 
-            @if (session('delete'))
-                <div class="alert alert-success" role="alert">
-                    {!!session('delete')!!}
-                </div>
-            @endif
-
             <h1 class="text-uppercase text-black-50">Lista dei Progetti per tipo</h1>
             <div class="table-container pt-2 w-75">
                 <table class="table">
